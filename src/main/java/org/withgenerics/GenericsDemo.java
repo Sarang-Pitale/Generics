01cf10b29fa7691823a625a22f543a87a20c7265
Sarang Pitale <87574631+Sarang-Pitale@users.noreply.github.com>
2023-12-09 11:18:39 +0530

    First Commit

---
+++ src/main/java/org/withgenerics/GenericsDemo.java
@@ -0,0 +1,11 @@
+package src.main.java.org.withgenerics;
+
+public class GenericsDemo {
+    public static void main(String[] args) {
+        Pair<String> person=new Pair<>("Ram","Shyam");
+        Pair<Integer> age=new Pair<>(10,12);
+        System.out.println("Age of "+person.getFirst()+" is "+age.getFirst());
+        System.out.println("Age of "+person.getSecond()+" is "+age.getSecond());
+
+    }
+}
---
+++ src/main/java/org/withoutgenerics/IntegerPair.java
@@ -0,0 +1,28 @@
+package src.main.java.org.withoutgenerics;
+
+public class IntegerPair {
+    private Integer first;
+    private Integer second;
+
+    public IntegerPair(Integer first, Integer second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public Integer getFirst() {
+        return first;
+    }
+
+    public void setFirst(Integer first) {
+        this.first = first;
+    }
+
+    public Integer getSecond() {
+        return second;
+    }
+
+    public void setSecond(Integer second) {
+        this.second = second;
+    }
+}
+
